68d9a41b4a059483b81b3a7c745f19c54a86a3ee
Richard Silverton <richsilv@yahoo.co.uk>
2014-01-24 13:46:35 +0000

    Corrected label alignment

--- Chart.js
+++ Chart.js
@@ -1098,7 +1098,7 @@ Chart = function(context){
 					ctx.translate(yAxisPosX + i*valueHop,xAxisPosY + config.scaleFontSize);
 					ctx.rotate(-(rotateLabels * (Math.PI/180)));
 					if (rotateLabels > 45)
-						ctx.translate(0, (valueHop + config.scaleFontSize) / 2);
+						ctx.translate(0, (valueHop - config.scaleFontSize) / 2);
 					ctx.fillText(data.labels[i], 0,0);
 					ctx.restore();
 				}
